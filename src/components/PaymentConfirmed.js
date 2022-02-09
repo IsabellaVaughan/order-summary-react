160ae66370d343d3e4959f9029d991a2e406c241
Isabella <isabella.vaughan@weareaccess.co.uk>
2022-02-09 12:23:15 +0000

    Payment form and confirmation of payment components added.

---
+++ src/components/PaymentConfirmed.js
@@ -0,0 +1,14 @@
+// Displays window confirming that the purchase was successful after clicking confirm
+
+import '../App.css';
+import './PaymentConfirmed.css';
+
+function PaymentConfirmed() {
+    return (
+        <div className="container" id="confirmation">
+            <p>Payment confirmed!</p>
+        </div>
+    )
+}
+
+export default PaymentConfirmed;
